125f341240c8e8ed4765740d448b29474e42abaf
inshaf ahamed <amdinshaf@gmail.com>
2021-07-28 00:18:52 +0530

    auth components added 🔐

--- src/App.js
+++ src/App.js
@@ -6,34 +6,115 @@ import "semantic-ui-css/semantic.min.css";
 import { useState, useEffect } from "react";
 import Loading from "./components/ui/Loading/Loading";
 import Sidebar from "./components/ui/Sidebar/Sidebar";
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Particles from "react-particles-js";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   const [IsLoading, setIsLoading] = useState(true);
+
+  const [IsAuth, setIsAuth] = useState(false);
+
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1500);
+    setTimeout(() => setIsLoading(false), 100);
   }, []);
   return IsLoading ? (
     <Loading />
   ) : (
     <Router>
       <div className="App">
-        <Sidebar />
-        <div  className="content">
-        <Switch>
-        <Route
-      path={`${process.env.PUBLIC_URL}/file-upload`}
-          component={FileUpload}
-      />
-      <Route
-      path={`${process.env.PUBLIC_URL}`}
-          component={Dashboard}
-      />
-        </Switch>
-        </div>
+        {IsAuth ? (
+          <>
+            <Sidebar />
+            <div className="content">
+              <Switch>
+                <Route
+                  path={`${process.env.PUBLIC_URL}/file-upload`}
+                  component={FileUpload}
+                />
+                <Route
+                  path={`${process.env.PUBLIC_URL}`}
+                  component={Dashboard}
+                />
+              </Switch>
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              style={{
+                width: "100vw",
+                height: "100vh",
+                background: "black",
+                position: "fixed",
+                top: "0",
+                left: "0",
+              }}
+            >
+              <div
+                style={{
+                  position: "absolute",
+                  width: "100vw",
+                  height: "100%",
+                }}
+              >
+                <Particles
+                height="100vh"
+                width="100vw"
+
+                  params={{
+                    particles: {
+                      number: {
+                        value: 160,
+                        density: {
+                          enable: false,
+                        },
+                      },
+                      size: {
+                        value: 5,
+                        random: true,
+                      },
+                      move: {
+                        direction: "bottom",
+                        out_mode: "out",
+                      },
+                      line_linked: {
+                        enable: false,
+                      },
+                    },
+                    interactivity: {
+                      events: {
+                        onclick: {
+                          enable: true,
+                          mode: "remove",
+                        },
+                      },
+                      modes: {
+                        remove: {
+                          particles_nb: 10,
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
+              <Switch>
+                <Route
+                  path={`${process.env.PUBLIC_URL}/auth/register`}
+                  component={Register}
+                />
+                <Route
+                  path={`${process.env.PUBLIC_URL}/auth/login`}
+                  component={Login}
+                />
+              </Switch>
+            </div>
+          </>
+        )}
       </div>
     </Router>
   );
-
 }
 
 function FileUpload() {
@@ -46,7 +127,5 @@ function FileUpload() {
     </Container>
   );
 }
-
-
 
 export default App;
